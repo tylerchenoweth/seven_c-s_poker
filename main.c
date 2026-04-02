b110f7907be492771570430f286a948bc9b8af96
Tyler Chenoweth <tchen358@gmail.com>
2026-04-02 04:36:24 +0000

    integrated new print_hand functions into main loop

--- main.c
+++ main.c
@@ -29,14 +29,15 @@ bool is_in_range(char c, char min, char max);
 // gameplay.c prototypes
 bool* redraw();
 struct Card* insertion_sort_hand(struct Card* hand);
-void print_hand(struct Card* hand);
+void print_hand_non_fancy(struct Card* hand);
 int determineWinner(struct Card* hand, int bet);
 int determineHand(struct Card* hand);
-
 
 // db_crud.c prototypes
 PlayerData db_main(void);
 
+// print_hand.c prototypes
+void print_hand(struct Card* hand);
 
 
 
