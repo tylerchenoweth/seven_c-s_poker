dbf1fad82fdd0f08cfd02dbc537064963d6914eb
Tyler Chenoweth <tchen358@gmail.com>
2026-04-06 02:22:56 +0000

    moved test to main

---
+++ cplusplus/main.cpp
@@ -0,0 +1,419 @@
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <stdexcept>
+#include <iterator>
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+#include <limits>
+
+using namespace std;
+
+
+class OneCard {
+    public:
+        OneCard() {
+            // cout << "card created..." << endl;
+        }
+
+        OneCard(int n) {
+            if(n >= 0 && n < 52) {
+                cout << "Card created..." << endl;
+                num = n;
+            } else {
+                throw invalid_argument("Must be number between 0-51");
+            }
+        }
+
+        int getNumIndex() {
+            return num % 13;
+        }
+
+        int getSuitIndex() {
+            return num % 4;
+        }
+
+        string getCardNum() {
+            return NUMS[getNumIndex()];
+        }
+
+        string getCardSuit() {
+            return SUITS[getSuitIndex()];
+        }
+
+        void setNum(int n) {
+            num = n;
+        }
+
+        int getNum() {
+            return num;
+        }
+
+    private:
+        int num;
+
+        const string SUITS[4] = {
+            "\u2660",
+            "\u2665",
+            "\u2666",
+            "\u2663"
+        };
+
+        const string NUMS[13] = {
+            "2", "3", "4", "5", "6",  
+            "7", "8", "9", "10",  
+            "J", "Q", "K", "A" 
+        };
+    
+};
+
+
+
+void printDeck(OneCard[]);
+void initiateCard(OneCard[]);
+void swapCards(OneCard[], int, int);
+void shuffleDeck(OneCard[]);
+void displayHand(OneCard[]);
+void holdOrDraw(OneCard[]);
+void sortHand(OneCard[]);
+int determineHand(OneCard[]);
+void handleBet(int&, int, int);
+bool isInteger(const string&);
+int getIntegerInput();
+
+
+
+
+int main() {
+    system("clear");
+
+    OneCard Deck[52];
+    
+    cout << "--------------------------------------------" << endl;
+    cout << "| WELCOME TO TYLER'S SEVEN C'S POKER GAME! |" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << endl;
+
+    int bankroll;
+    int betAmount;
+    int pokerHandRank;
+    string strBetAmount;
+    bool playAgain = true;
+
+    cout << "ENTER SEED MONEY \n$";
+    do {
+        bankroll = getIntegerInput();
+    } while(bankroll <= 0);
+
+    while(playAgain == true) {
+        
+        string playAgainInput;
+
+        cout << endl << "HOW MUCH DO YOU WANT TO BET \n$";
+        do {
+            betAmount = getIntegerInput();
+        } while(bankroll <= 0);
+
+        initiateCard(Deck);
+        printDeck(Deck);
+        shuffleDeck(Deck);
+        // printDeck(Deck);
+        sortHand(Deck);
+        displayHand(Deck);
+        holdOrDraw(Deck);
+        sortHand(Deck);
+        displayHand(Deck);
+        pokerHandRank = determineHand(Deck);
+        handleBet(bankroll, betAmount, pokerHandRank);
+
+        while(
+            playAgainInput != "y" && playAgainInput != "Y" &&
+            playAgainInput != "n" && playAgainInput != "N" 
+        ) {
+            cout << "Do you want to play again(y/N)? \n> ";
+            cin >> playAgainInput;
+        }
+
+        if(playAgainInput == "n" || playAgainInput == "N")
+            playAgain = false;
+
+        // clear anything leftover on the input line
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    cout << endl << endl << "bye bye..." << endl << endl;
+}
+
+
+void printDeck(OneCard Deck[]) {
+    for(int i=0; i<52; i++) {
+        if(i%13 == 0 and i != 0)    
+            cout << endl;
+        else if(i == 5)
+            cout << endl;
+        else if(i == 10)
+            cout << endl;
+        cout << Deck[i].getCardNum() << Deck[i].getCardSuit() << " "; 
+    }
+    cout << endl << endl;
+}
+
+
+void initiateCard(OneCard Deck[]) {
+    for(int i=0; i<52; i++) {
+        Deck[i].setNum(i);
+    }
+}
+
+
+void swapCards(OneCard Deck[], int index_1, int index_2) {
+    int tmpNum = Deck[index_1].getNum();
+    Deck[index_1].setNum(Deck[index_2].getNum());
+    Deck[index_2].setNum(tmpNum);
+}
+
+
+void shuffleDeck(OneCard Deck[]) {
+    random_device rd;  // Non-deterministic seed source
+    mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+    uniform_int_distribution<> distrib(0, 51);
+
+    for(int i=0; i<100; i++) {
+        int randNum_1 = distrib(gen);
+        int randNum_2 = distrib(gen);
+
+        swapCards(Deck, randNum_1, randNum_2);
+    }
+}
+
+
+void displayHand(OneCard Deck[]) {
+    cout << "-----------------------------" << endl;
+    for(int i=0; i<5; i++) {
+        cout << Deck[i].getCardNum() << Deck[i].getCardSuit() << " - ";
+    }
+    cout << "|\n-----------------------------" << endl;
+    cout << endl << endl;
+}
+
+
+void holdOrDraw(OneCard Deck[]) {
+    string hand;
+    bool allCards = false;
+    bool someCards = false;
+    bool isValidInput = true;
+    bool cardsToRedraw[5] = {false, false, false, false, false};
+
+    // cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    do
+    {
+        isValidInput = true;
+        allCards = false;
+        someCards = false;
+        int count = 0;
+
+        cout << "Redraw? Enter 1-5 or 0 for all\n> ";
+        getline(cin, hand);
+        
+        // reset bool array
+        for(int i=0; i<5; i++)
+            cardsToRedraw[i] = false;
+
+        for(int i=0; i<hand.length(); i++) {
+            if(hand[i] >= '1' && hand[i] <= '5') {
+                someCards = true;
+                count += 1;
+                if(cardsToRedraw[(hand[i]-'0')-1] == true) 
+                    isValidInput = false;
+                else 
+                    cardsToRedraw[(hand[i]-'0')-1] = true;
+            } else if(hand[i] == '0') {
+                allCards = true;
+
+                for(int k=0; k<5; k++) 
+                    cardsToRedraw[k] = true; 
+            }
+            else if(hand[i] == ' ') 
+                continue;
+            else 
+                isValidInput = false;
+
+            if((someCards == true && allCards == true) || isValidInput == false)
+                isValidInput = false;
+        }
+
+        if(isValidInput == false) 
+            cout << "Invalid input..." << endl;
+
+    } while(isValidInput == false);
+
+    int newCardIndex = 5;
+
+    // deal new hand
+    for(int i=0; i<5; i++) {
+        if(cardsToRedraw[i] == true) {
+            swapCards(Deck, i, newCardIndex);
+            newCardIndex++;
+        }
+    }
+}
+
+
+void sortHand(OneCard Deck[]) {
+    for(int i=1; i<5; i++) {
+        OneCard Card = Deck[i];
+        int k = i - 1;
+        while(k >= 0 && Deck[k].getNumIndex() > Card.getNumIndex()) {
+            Deck[k+1].setNum(Deck[k].getNum());
+            k--;
+            Deck[k+1].setNum(Card.getNum());
+        }
+    }
+}
+
+
+bool isPair(OneCard Card_1, OneCard Card_2) {
+    if(Card_1.getNumIndex() == Card_2.getNumIndex())
+        return true;
+    else
+        return false;
+}
+
+
+int determineHand(OneCard Deck[]) {
+
+    bool isFlush = true;
+    bool isStraight = true;
+    int fourCount = 0;
+
+    for(int i=0; i<4; i++) {
+        if(Deck[i].getNumIndex()+1 != (Deck[i+1].getNumIndex())) {
+            isStraight = false;
+        }
+        if(Deck[i].getSuitIndex() != Deck[i+1].getSuitIndex()) {
+            isFlush = false;
+        }
+        if(Deck[i].getNumIndex() == (Deck[i+1].getNumIndex())) {
+            fourCount += 1;
+        } else if(fourCount != 3) {
+            fourCount = 0;
+        }
+    }
+
+    if (isStraight == true && isFlush == true) {
+        if (Deck[0].getNumIndex() == 8) {
+            cout << "--ROYAL FLUSH--" << endl;
+            return 9;
+        } else {
+            cout << "--STRAIGHT FLUSH--" << endl;
+            return 8;
+        }
+    } 
+    else if (fourCount == 3) {
+        cout << "--FOUR OF A KIND--" << endl;
+        return 7;
+    } 
+    else if (isPair(Deck[0], Deck[1]) && isPair(Deck[0], Deck[2]) && isPair(Deck[3], Deck[4])) {
+        cout << "--FULL HOUSE--" << endl;
+        return 6;
+    } 
+    else if (isPair(Deck[2], Deck[3]) && isPair(Deck[2], Deck[4]) && isPair(Deck[0], Deck[1])) {
+        cout << "--FULL HOUSE--" << endl;
+        return 6;
+    } 
+    else if (isFlush == true) {
+        cout << "--FLUSH--" << endl;
+        return 5;
+    } 
+    else if (isStraight == true) {
+        cout << "--STRAIGHT--" << endl;
+        return 4;
+    } 
+    else {   
+        for (int i = 0; i < 3; i++) {
+            if (isPair(Deck[i], Deck[i+1]) && isPair(Deck[i], Deck[i+2])) {
+                cout << "--THREE OF A KIND--" << endl;
+                return 3;
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            if (isPair(Deck[i], Deck[i+1])) {
+                for (int k = (i + 2); k < 4; k++) {
+                    if (isPair(Deck[k], Deck[k+1])) {
+                        cout << "--TWO PAIR--" << endl;
+                        return 2;
+                    }
+                }
+            }
+        }
+                  
+        for (int i = 0; i < 4; i++) {
+            if (isPair(Deck[i], Deck[i+1])) {
+                cout << "--ONE PAIR--" << endl;
+                return 1;
+            }
+        }
+    } 
+    
+    cout << "--Nothing--" << endl;
+    return 0;
+}
+
+
+void handleBet(int& bankroll, int betAmount, int pokerHandRank) {
+    int oldBankroll = bankroll;
+
+    if(pokerHandRank == 9)
+        bankroll += (betAmount * 100);
+    else if(pokerHandRank == 8)
+        bankroll += (betAmount * 50);
+    else if(pokerHandRank == 7)
+        bankroll += (betAmount * 20);
+    else if(pokerHandRank == 6)
+        bankroll += (betAmount * 7);
+    else if(pokerHandRank == 5)
+        bankroll += (betAmount * 5);
+    else if(pokerHandRank == 4)
+        bankroll += (betAmount * 4);
+    else if(pokerHandRank == 3)
+        bankroll += (betAmount * 3);
+    else if(pokerHandRank == 2)
+        bankroll += (betAmount * 2);
+    else if(pokerHandRank == 1)
+        bankroll += (betAmount * 1);
+    else 
+        bankroll -= betAmount;
+
+    if(bankroll > oldBankroll)
+        cout << "\nYou won: $" << (bankroll-oldBankroll) << endl;
+    else
+        cout << "\nYou lost: $" << betAmount << endl;
+    cout << "NEW BALANCE: $" << bankroll << endl;
+}
+
+bool isInteger(const string& s) {
+    stringstream ss(s);
+    int n;
+    // Check if we can extract an int AND if nothing else is left in the string
+    return (ss >> n) && (ss >> ws).eof();
+}
+
+
+int getIntegerInput() {
+    string input;
+    int num;
+    
+    while (true) {
+        getline(cin, input);
+        stringstream ss(input);
+
+        if (ss >> num && ss.eof()) {
+            return num;
+        }
+
+        cout << "Invalid input. Try again\n> $";
+    }
+}
--- cplusplus/test.cpp
+++ cplusplus/test.cpp
@@ -1,14 +1,8 @@
-
 #include <iostream>
 #include <string>
-#include <random>
-#include <stdexcept>
-#include <iterator>
-#include <cstring>
-#include <cstdlib>
-
+#include <limits>
+#include <iomanip>
 using namespace std;
-
 
 class OneCard {
     public:
@@ -63,155 +57,115 @@ class OneCard {
             "2", "3", "4", "5", "6",  
             "7", "8", "9", "10",  
             "J", "Q", "K", "A" 
-        };
-    
+        }; 
 };
 
 
 
-void printDeck(OneCard[]);
-void initiateCard(OneCard[]);
-void shuffleDeck(OneCard[]);
-void displayHand(OneCard[]);
-void holdOrDraw(OneCard[]);
-void sortHand(OneCard[]);
 
 
-int main() {
-    system("clear");
-
-    OneCard Deck[52];
-    
-
-    holdOrDraw(Deck);
 
 
-    exit(0);
 
 
-    cout << "--------------------------------------------" << endl;
-    cout << "| WELCOME TO TYLER'S SEVEN C'S POKER GAME! |" << endl;
-    cout << "--------------------------------------------" << endl;
-    cout << endl;
 
-    initiateCard(Deck);
-    printDeck(Deck);
-    shuffleDeck(Deck);
-    cout << endl << endl;
-    printDeck(Deck);
-    displayHand(Deck);
-    sortHand(Deck);
-    displayHand(Deck);
 
-    cout << endl << endl << "bye bye..." << endl << endl;
-}
 
 
 void printDeck(OneCard Deck[]) {
-    for(int i=0; i<52; i++) {
-        if(i%13 == 0 and i != 0)    
-            cout << endl;
-        cout << Deck[i].getCardNum() << Deck[i].getCardSuit() << " "; 
-    }
-    cout << endl << endl;
-}
-
-
-void initiateCard(OneCard Deck[]) {
-    for(int i=0; i<52; i++) {
-        Deck[i].setNum(i);
+    for(int i=0; i<5; i++) {
+        cout << Deck[i].getCardNum() << Deck[i].getCardSuit() << endl;
     }
 }
 
 
-void shuffleDeck(OneCard Deck[]) {
-    random_device rd;  // Non-deterministic seed source
-    mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-    uniform_int_distribution<> distrib(0, 51);
 
-    for(int i=0; i<100; i++) {
-        int randNum_1 = distrib(gen);
-        int randNum_2 = distrib(gen);
-
-        int tmpNum = Deck[randNum_1].getNum();
-        Deck[randNum_1].setNum(Deck[randNum_2].getNum());
-        Deck[randNum_2].setNum(tmpNum);
-    }
-}
 
 
 void displayHand(OneCard Deck[]) {
-    for(int i=0; i<5; i++) {
-        cout << Deck[i].getCardNum() << Deck[i].getCardSuit() << " - ";
+    int cardLines[13][4] = {
+        {0, 2, 3, 4},
+        {0, 2, 3, 8},
+        {0, 2, 3, 12},
+        {0, 2, 3, 16},
+        {0, 2, 3, 20},
+        {0, 2, 3, 24},
+        {0, 2, 3, 28},
+        {0, 2, 3, 32},
+        {0, 2, 3, 36},
+        {0, 2, 3, 40},
+        {0, 2, 3, 44},
+        {0, 2, 3, 44},
+        {0, 2, 3, 44}
+    };
+
+
+    for(int i=0; i<4; i++) {
+        // go through cards
+        for(int k=0; k<5; k++) {
+            cout << cardLines[Deck[k].getNumIndex()][i] << " ";
+        }
+        cout << endl;
     }
+
+    int space = 3;
+
     cout << endl;
-}
-
-
-void holdOrDraw(OneCard Deck[]) {
 
 
 
+    // go through lines
+    for(int i=0; i<4; i++) {
+        // go through cards
+        for(int k=0; k<5; k++) {
+            // cout << cardLines[Deck[k].getNumIndex()][i] << " ";
+            string num = Deck[k].getCardNum();
+            string suit = Deck[k].getCardSuit();
 
-    string hand;
-    bool allCards = false;
-    bool someCards = false;
-    bool isValidInput = true;
-    
-    
-
-    do
-    {
-        isValidInput = true;
-
-        cout << "Redraw? Enter 1-5 or 0 for all\n> ";
-        cin >> hand;
-        
-        int count = 0;
-        bool cardsToRedraw[5] = {false, false, false, false, false};
-
-        for(int i=0; i<hand.length(); i++) {
-            // cout << "Inside loop: " << hand[i] << endl;
-            if(hand[i] >= '1' && hand[i] <= '5') {
-                someCards = true;
-                count += 1;
-                if(cardsToRedraw[(hand[i]-'0')-1] == true) {
-                cout << "error: 2" << endl;
-                    isValidInput = false;}
-            } else if(hand[i] == '0') {
-                cout << "error: 2" << endl;
-                allCards = true;
-            } else if(hand[i] == ' ') {
-                cout << "error: 2" << endl;
-                continue;
-            } else {
-                cout << "error: 2" << endl;
-                isValidInput = false;
-            }
-
-            if((someCards == true && allCards == true) || count > 5) 
-                isValidInput = false;
-
-            
-
-
+            switch(cardLines[Deck[k].getNumIndex()][i]) 
+            {
+                case 0:
+                    cout << "-----------" << setw(space);
+                    break;
+                case 1:
+                    cout << "|" << setw(9) << "|" << setw(space);
+                    break;
+                case 2:
+                    cout << "|" << num << setw(8) << "|" << setw(space);
+                    break;
+                case 3:
+                    cout << "|" << setw(8) << num << "|" << setw(space);
+                    break;
+                default:
+                    cout << "/";
+                    break;
+                    
+            }; 
         }
-
-        if(isValidInput == false) 
-                cout << "Invalid input..." << endl;
-
-    } while(isValidInput == false);
-}
-
-
-void sortHand(OneCard Deck[]) {
-    for(int i=1; i<5; i++) {
-        int value = Deck[i].getNumIndex();
-        int k = i - 1;
-        while(k >= 0 && Deck[k].getNumIndex() > value) {
-            Deck[k+1].setNum(Deck[k].getNum());
-            k--;
-            Deck[k+1].setNum(value);
-        }
+        cout << setw(0) << endl;
     }
+
+
+
+
+}
+
+
+
+
+
+
+
+int main() {
+    int cards[5] = {0, 5, 8, 11, 12};
+    OneCard Deck[5];
+
+    for(int i=0; i<5; i++) {
+        Deck[i].setNum(cards[i]);
+    }
+
+    printDeck(Deck);
+    displayHand(Deck);
+
+
 }
